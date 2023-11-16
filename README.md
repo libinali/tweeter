52cc07b9c429fdb5ef7b106c6c3917d678e8ebd6
libinali <ali.libin@yahoo.com>
2023-11-16 06:39:03 +0000

    Updated Readme.md

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 # Tweeter Project
 
-Tweeter is a simple, single-page Twitter clone.
+Tweeter is a simple, single-page Twitter clone, that allows users to post tweets to a Tweet feed. This app is built with HTML, CSS, JS, jQuery, and AJAX on the front end, and Node.js and Express on the back end. Tweeter is a responsive app designed to function on desktops, tablets or mobile devices.
 
-This repository is the starter code for the project: Students will fork and clone this repository, then build upon it to practice their HTML, CSS, JS, jQuery and AJAX front-end skills, and their Node, Express back-end skills.
+# Final Product 
+![]()
 
 ## Getting Started
 
-1. [Create](https://docs.github.com/en/repositories/creating-and-managing-repositories/creating-a-repository-from-a-template) a new repository using this repository as a template.
+1. Create a new repository using this repository as a template.
 2. Clone your repository onto your local device.
-3. Install dependencies using the `npm install` command.
-3. Start the web server using the `npm run local` command. The app will be served at <http://localhost:8080/>.
-4. Go to <http://localhost:8080/> in your browser.
+3. Install dependencies using the npm install command.
+4. Start the web server using the npm run local command. The app will be served at http://localhost:8080/.
+5. Go to http://localhost:8080/ in your browser.
 
 ## Dependencies
 
 - Express
 - Node 5.10.x or above
+- Chance
+- Body-parser
+- Nodemon
+- Timeago.js
+- Md5
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,15 +2,15 @@
 $(document).ready(function() {
 
   // Escape function to prevent XSS
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-// Function to create a tweet element
-const createTweetElement = function(tweetData) {
-  const $tweet = $(`          
+  // Function to create a tweet element
+  const createTweetElement = function(tweetData) {
+    const $tweet = $(`          
   <article>
     <header>
       <div class="user-info">
@@ -37,80 +37,80 @@ const createTweetElement = function(tweetData) {
     </footer>
   </article>`);
 
-  return $tweet;
-}
+    return $tweet;
+  };
 
-// Function to render tweets on the page
-const renderTweets = function(tweets) {
-  const $tweetsContainer = $('.tweets-history');
-  $tweetsContainer.empty();
-  for (const tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    $('.tweets-history').prepend($tweet);
-  }
-}
-
-// Function to load tweets from the server
-const loadTweets = function() {
-  $.ajax({
-    url: '/tweets',
-    method: 'GET',
-    dataType: 'json',
-    success: (tweets) => {
-      renderTweets(tweets);
-    },
-    error: (error) => {
-      console.error(error);
+  // Function to render tweets on the page
+  const renderTweets = function(tweets) {
+    const $tweetsContainer = $('.tweets-history');
+    $tweetsContainer.empty();
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('.tweets-history').prepend($tweet);
     }
-  });  
-};
+  };
 
-// Load tweets on page load
-loadTweets();
+  // Function to load tweets from the server
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json',
+      success: (tweets) => {
+        renderTweets(tweets);
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  };
+
+  // Load tweets on page load
+  loadTweets();
 
 
-// Form submission handling
-$("#tweets-form").on("submit", function(e) {
-  e.preventDefault();
+  // Form submission handling
+  $("#tweets-form").on("submit", function(e) {
+    e.preventDefault();
   
-  // Hide the error element on form submission
-  $(".error-message").slideUp();
+    // Hide the error element on form submission
+    $(".error-message").slideUp();
 
-  // Get tweet content
-  const tweetContent = $("#tweet-text").val();
+    // Get tweet content
+    const tweetContent = $("#tweet-text").val();
 
-  // Validation checks
-  if (!tweetContent) {
-    $(".error-message").text("⚠️ Please enter a tweet before submitting ⚠️").slideDown();
-    return; 
-  }
-
- if (tweetContent.length > 140) {
-    $(".error-message").text("⚠️ Tweet is too long. Maximum character limit is 140. ⚠️").slideDown();
-    return; 
-  }
-
-  // Serialize form data
-  const formData = $(this).serialize();
-
-  // AJAX request to submit tweet
-  $.ajax({
-    method: "POST",
-    url: "/tweets",
-    data: formData,
-    contentType: "application/x-www-form-urlencoded; charset=UTF-8",
-    success: function(data) {
-      // Reload tweets and reset form
-      loadTweets();
-      $("#tweet-text").val("");
-      $('.counter').text(140);
-    },
-    error: function(error) {
-      console.error(error);
+    // Validation checks
+    if (!tweetContent) {
+      $(".error-message").text("⚠️ Please enter a tweet before submitting ⚠️").slideDown();
+      return;
     }
-  });
 
-});
+    if (tweetContent.length > 140) {
+      $(".error-message").text("⚠️ Tweet is too long. Maximum character limit is 140. ⚠️").slideDown();
+      return;
+    }
+
+    // Serialize form data
+    const formData = $(this).serialize();
+
+    // AJAX request to submit tweet
+    $.ajax({
+      method: "POST",
+      url: "/tweets",
+      data: formData,
+      contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+      success: function(data) {
+      // Reload tweets and reset form
+        loadTweets();
+        $("#tweet-text").val("");
+        $('.counter').text(140);
+      },
+      error: function(error) {
+        console.error(error);
+      }
+    });
+
+  });
 
 });
 
